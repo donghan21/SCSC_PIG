cf50e892730d28f64ee223a66e1b19ced713449d
Mouse7276 <jangjunhee1@snu.ac.kr>
2023-11-27 13:33:43 +0900

    Add reservationpage

--- lib/reservationpage/reservationpage_model.dart
+++ lib/reservationpage/reservationpage_model.dart
@@ -7,14 +7,17 @@ import '/utils/model_utils.dart';
 class ReservationPageModel extends Model {
   final unFocusNode = FocusNode();
 
+  DateTime? selectedDay = DateTime.now();
+  TextEditingController? reasonController;
+
   @override
   void initState(BuildContext context) {
-
   }
 
   @override
   void dispose() {
     unFocusNode.dispose();
+    reasonController?.dispose();
   }
 
 }
